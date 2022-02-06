dbffb99fc1a62d4622b64687398c84fa0a9640f9
ANADIR <amitnadir@gmail.com>
2022-02-06 12:34:11 +0200

    update package class

--- PingOneSDKPackage/Package.swift
+++ PingOneSDKPackage/Package.swift
@@ -9,16 +9,16 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "PingOneSDK",
-            targets: ["PingOneSDK"]),
+            name: "PingOneSDKPackage",
+            targets: ["PingOneSDKPackage"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .binaryTarget(name: "PingOneSDK",
-                      url: "https://github.com/ANADIR/SPMPingOne/blob/main/PingOneSDK/PingOneSDK.xcframework.zip",
+        .binaryTarget(name: "PingOneSDKPackage",
+                      url: "https://github.com/ANADIR/ANADIR-SPMPingOne/blob/main/PingOneSDKPackage/PingOneSDK.xcframework.1.7.0.zip",
                       checksum: "581c8a60bf0181641966b7b3c70eacb0f801a6f174abf3c5b18efc1e99c4dd43"),
     ]
 )
